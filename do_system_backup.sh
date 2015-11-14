3633329dd14d9133ffdafa1594deed2207e77c5a
Jeff Coffler <jeffaco@users.noreply.github.com>
2015-11-13 17:45:32 -0800

    Ignore .git repo files on git repositories (we can recover that from server)

--- do_system_backup.sh
+++ do_system_backup.sh
@@ -2,9 +2,11 @@
 
 cd ~/
 
-tar czvf /tmp/base-backup.tar.gz \
-    --exclude .rbenv --exclude base-backup.tar.gz --exclude dev \
-    --exclude pbuild_logs --exclude Miscellaneous/archive \
+# Don't back up stuff from git repositories (but include enough to re-seed git)
+
+tar czvhf /tmp/base-backup.tar.gz \
+    --exclude .rbenv --exclude base-backup.tar.gz --exclude dev --exclude pbuild_logs \
+    --exclude bin/.git --exclude miscellaneous/.git --exclude test/.git \
     .
 
 
