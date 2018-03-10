fe578a9e934c1f2a13c3f7d896655ad99a92fa23
Jeff Coffler <jeff@taltos.com>
2018-03-09 16:34:43 -0800

    do_system_backup.sh: Trim unnecessary files for backup

--- do_system_backup.sh
+++ do_system_backup.sh
@@ -5,7 +5,13 @@ cd ~/
 # Don't back up stuff from git repositories (but include enough to re-seed git)
 
 tar czvhf /tmp/base-backup.tar.gz \
-    --exclude .rbenv --exclude base-backup.tar.gz \
+    --exclude .azure --exclude .cache --exclude .ccache --exclude .cmake \
+    --exclude .config --exclude .dbus --exclude .esd_auth --exclude .gnupg \
+    --exclude .ICEauthority --exclude .local --exclude .m2 --exclude .mozilla \
+    --exclude .pki --exclude .pyenv --exclude .python-eggs \
+    --exclude .rbenv --exclude .rbtools-cookies --exclude .rnd \
+    --exclude .rpmmacros --exclude .subversion --exclude .Xauthority \
+    --exclude base-backup.tar.gz \
     --exclude bin --exclude dev --exclude miscellaneous --exclude pbuild_logs \
     --exclude test --exclude .microsoft \
     .
