bba77ac86c074400a58d7c267da26df23f038db0
Jeff Coffler <jeff@taltos.com>
2018-06-06 16:41:52 +0000

    build-hana.sh: Add option to clean out docker containers

--- build-hana.sh
+++ build-hana.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Build HANA project and run unit tests
 
 if [ ! -f ./build/build.sh ]; then
@@ -7,6 +9,29 @@ fi
 
 set -e
 
+if [ $# -gt 0 ]; then
+    if [ $# -eq 1 -a "$1" == "cleanup" ]; then
+	echo "#################### Cleaning out Docker ####################"
+
+	# Delete all containers
+	CONTAINERS="$(docker ps -a -q)"
+	[ -n "$CONTAINERS" ] && docker rm -f $CONTAINERS
+
+	# Delete all images
+	IMAGES="$(docker images -q)"
+	[ -n "$IMAGES" ] && docker rmi -f $IMAGES
+
+	exit 0
+    fi
+
+    echo "Bad parameter: Use \"cleanup\" to clean up docker" 1 >& 2
+    exit 1
+fi
+
+echo "#################### Verifying docker container ####################"
+scripts/devenv.sh
+
+echo
 echo "#################### Performing build/unit tests ####################"
 ./build/build.sh -K
 
