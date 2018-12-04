9207faf4ec5dac883541ac4bc8f3e2311ab1a157
Jeff Coffler <jeff@taltos.com>
2018-12-03 18:21:44 -0800

    build-hana.sh: Changes to support builds on Mac OS/X

--- build-hana.sh
+++ build-hana.sh
@@ -9,23 +9,33 @@ fi
 
 set -e
 
+OS=$(uname -s)
+case $OS in
+    Darwin | Linux)
+        ;;
+
+    *)
+        echo "Unsupported Operating System: $OS"
+        exit 1
+esac
+
 if [ $# -gt 0 ]; then
-    if [ $# -eq 1 -a "$1" == "cleanup" ]; then
-	echo "#################### Cleaning out Docker ####################"
+    if [ $# -eq 1 -a "$1" == "--cleanup" ]; then
+        echo "#################### Cleaning out Docker ####################"
 
-	# Delete all containers
-	CONTAINERS="$(docker ps -a -q)"
-	[ -n "$CONTAINERS" ] && docker rm -f $CONTAINERS
+        # Delete all containers
+        CONTAINERS="$(docker ps -a -q)"
+        [ -n "$CONTAINERS" ] && docker rm -f $CONTAINERS
 
-	# Delete all images
-	IMAGES="$(docker images -q)"
-	[ -n "$IMAGES" ] && docker rmi -f $IMAGES
+        # Delete all images
+        IMAGES="$(docker images -q)"
+        [ -n "$IMAGES" ] && docker rmi -f $IMAGES
 
-	echo
-	exit 0
+        echo
+        exit 0
     fi
 
-    echo "Bad parameter: Use \"cleanup\" to clean up docker" 1 >& 2
+    echo "Bad parameter: Use \"--cleanup\" to clean up docker" 1 >& 2
     exit 1
 fi
 
@@ -36,6 +46,8 @@ echo
 echo "#################### Performing build/unit tests ####################"
 ./build/build.sh -K
 
-echo
-echo "#################### Running BVT tests ####################"
-./bvt/hanarp/runTests.sh 
+if [ $OS = "Linux" ]; then
+    echo
+    echo "#################### Running BVT tests ####################"
+    ./bvt/hanarp/runTests.sh 
+fi
